b50e843acf22522e5084c73a0a50c42f2b8c6117
Wallace <wallace.cardoso206@gmail.com>
2026-03-28 21:42:29 -0300

    Gráfico de barras funcional

--- frontend/empresa/perfil/perfil-empresa.js
+++ frontend/empresa/perfil/perfil-empresa.js
@@ -5,7 +5,9 @@ if (localStorageCandidatos) {
 }
 const tabela = document.getElementById('tabelaCandidatos');
 tabela.innerHTML = '';
+const qtdCompetencias = {}; //Um obj vazio q vai guardar a qtd de competencias no padrão "{python: 3    Java:2}"
 listaCandidatos.forEach((candidato) => {
+    //Tabela
     const linha = `
         <tr>
             <td>${candidato.estado}</td>
@@ -14,6 +16,23 @@ listaCandidatos.forEach((candidato) => {
         </tr>
     `;
     tabela.innerHTML += linha;
+    //Gráfico de barras
+    candidato.competencias.forEach((competencia) => {
+        qtdCompetencias[competencia] = (qtdCompetencias[competencia] || 0) + 1; //Se a competencia tá no obj add 1 || se não tá inicia com 0 e add 1
+    });
+});
+const nomes = Object.keys(qtdCompetencias);
+const quantidades = Object.values(qtdCompetencias);
+const ctx = document.getElementById('graficoCompetencias');
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: nomes,
+        datasets: [{
+                label: 'Quantidade de Candidatos por Competência',
+                data: quantidades
+            }]
+    }
 });
 export {};
 //# sourceMappingURL=perfil-empresa.js.map
--- frontend/empresa/perfil/perfil-empresa.ts
+++ frontend/empresa/perfil/perfil-empresa.ts
@@ -1,5 +1,5 @@
 import type { Empresa } from "../../empresa/cadastro/cadastro-empresa";
-
+declare var Chart: any; // Forma mais simples de evitar o erro de new Chart
 
 const localStorageCandidatos = localStorage.getItem('candidatos');
 let listaCandidatos: Empresa[] = [];
@@ -12,8 +12,10 @@ const tabela = document.getElementById('tabelaCandidatos') as HTMLTableElement;
 tabela.innerHTML = '';
 
 
-
+const qtdCompetencias: { [key: string]: number } = {}; //Um obj vazio q vai guardar a qtd de competencias no padrão "{python: 3    Java:2}"
 listaCandidatos.forEach((candidato) => {
+
+    //Tabela
     const linha = `
         <tr>
             <td>${candidato.estado}</td>
@@ -21,6 +23,29 @@ listaCandidatos.forEach((candidato) => {
             <td>${candidato.competencias.join(' | ')}</td>
         </tr>
     `;
-
     tabela.innerHTML += linha;
+    
+
+    //Gráfico de barras
+    candidato.competencias.forEach((competencia) => {
+        qtdCompetencias[competencia] = (qtdCompetencias[competencia] || 0) + 1; //Se a competencia tá no obj add 1 || se não tá inicia com 0 e add 1
+    });
+
+});
+
+const nomes = Object.keys(qtdCompetencias);
+const quantidades = Object.values(qtdCompetencias);
+
+
+const ctx = document.getElementById('graficoCompetencias') as HTMLCanvasElement;
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: nomes,
+        datasets: [{
+            label: 'Quantidade de Candidatos por Competência',
+            data: quantidades
+        }]
+    }
 });

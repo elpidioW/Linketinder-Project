159c70efbc1e9ca8563d032b8e0c899601b5f89a
Wallace <wallace.cardoso206@gmail.com>
2026-04-09 18:45:11 -0300

    Classe Endereco

---
+++ backend/src/main/groovy/org/example/model/Endereco.groovy
@@ -0,0 +1,25 @@
+package org.example.model
+
+class Endereco{
+    Integer id
+    String cep
+    String cidade
+    String estado
+    String pais
+
+    Endereco(Integer id, String cep, String cidade, String estado, String pais) {
+        this.id = id
+        this.cep = cep
+        this.cidade = cidade
+        this.estado = estado
+        this.pais = pais
+    }
+
+    public void exibirEndereco() {
+        println("ID: $id")
+        println("CEP: $cep")
+        println("Cidade: $cidade")
+        println("Estado: $estado")
+        println("Pais: $pais")
+    }
+}
